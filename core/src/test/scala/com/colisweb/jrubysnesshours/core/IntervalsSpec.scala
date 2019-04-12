7d038ac56b20b36b7f8560df96c9fd9b53cf2912
jules Ivanic <jules.ivanic@gmail.com>
2019-04-12 17:25:17 +0200

    Clean

--- core/src/test/scala/com/colisweb/jrubysnesshours/core/IntervalsSpec.scala
+++ core/src/test/scala/com/colisweb/jrubysnesshours/core/IntervalsSpec.scala
@@ -7,9 +7,7 @@ import org.scalatest.{Matchers, WordSpec}
 
 class IntervalsSpec extends WordSpec with Matchers {
 
-  implicit class StringToLocalTime(str: String) {
-    def toLocalTime: LocalTime = LocalTime.parse(str)
-  }
+  import SpecUtils._
 
   val planning: Map[DayOfWeek, List[TimeInterval]] = Map(
     MONDAY -> List(TimeInterval("09:00".toLocalTime, "19:00".toLocalTime)),
@@ -27,207 +25,6 @@ class IntervalsSpec extends WordSpec with Matchers {
   )
   val zoneId: ZoneId = ZoneId.of("Europe/Paris")
 
-  "Schedule" should {
-
-    "without exceptions" should {
-
-      val schedule = Schedule(planning, Map.empty[LocalDate, List[TimeInterval]], zoneId)
-
-      "compute 2 intervals between Thursday 18:00 to Friday 10:00" in {
-        val s1 = schedule.intervalsBetween(
-          parseZonedDateTime("2019-03-21", "18:00"),
-          parseZonedDateTime("2019-03-22", "10:00")
-        )
-        s1 should contain theSameElementsInOrderAs List(
-          aTimeIntervalForDate("2019-03-21", "18:00", "19:00"),
-          aTimeIntervalForDate("2019-03-22", "09:30", "10:00")
-        )
-      }
-      "compute 2 intervals between Saturday 13:00 to same Saturday 16:00" in {
-        val s2 =
-          schedule.intervalsBetween(
-            parseZonedDateTime("2019-03-23", "13:00"),
-            parseZonedDateTime("2019-03-23", "16:00")
-          )
-        s2 should contain theSameElementsInOrderAs List(
-          aTimeIntervalForDate("2019-03-23", "13:00", "14:00"),
-          aTimeIntervalForDate("2019-03-23", "15:00", "16:00")
-        )
-      }
-      "compute 3 intervals between Saturday 13:00 to Monday 10:00" in {
-        val s3 =
-          schedule.intervalsBetween(
-            parseZonedDateTime("2019-03-23", "13:00"),
-            parseZonedDateTime("2019-03-25", "10:00")
-          )
-        s3 should contain theSameElementsInOrderAs List(
-          aTimeIntervalForDate("2019-03-23", "13:00", "14:00"),
-          aTimeIntervalForDate("2019-03-23", "15:00", "19:00"),
-          aTimeIntervalForDate("2019-03-25", "09:00", "10:00")
-        )
-      }
-      "compute 5 intervals betweenSaturday 13:00 to Tuesday 16:00" in {
-        val s4 =
-          schedule.intervalsBetween(
-            parseZonedDateTime("2019-03-23", "13:00"),
-            parseZonedDateTime("2019-03-26", "16:00")
-          )
-        s4 should contain theSameElementsInOrderAs List(
-          aTimeIntervalForDate("2019-03-23", "13:00", "14:00"),
-          aTimeIntervalForDate("2019-03-23", "15:00", "19:00"),
-          aTimeIntervalForDate("2019-03-25", "09:00", "19:00"),
-          aTimeIntervalForDate("2019-03-26", "09:30", "14:00"),
-          aTimeIntervalForDate("2019-03-26", "15:00", "16:00")
-        )
-      }
-      "compute 2 intervals between Sunday 13:00 to Tuesday 10:00" in {
-        val s5 =
-          schedule.intervalsBetween(
-            parseZonedDateTime("2019-03-24", "13:00"),
-            parseZonedDateTime("2019-03-26", "10:00")
-          )
-        s5 should contain theSameElementsInOrderAs List(
-          aTimeIntervalForDate("2019-03-25", "09:00", "19:00"),
-          aTimeIntervalForDate("2019-03-26", "09:30", "10:00")
-        )
-      }
-      "compute 8 intervals between Monday 09:00 to Sunday 23:00" in {
-        val s6 =
-          schedule.intervalsBetween(
-            parseZonedDateTime("2019-03-18", "09:00"),
-            parseZonedDateTime("2019-03-24", "23:00")
-          )
-        s6 should contain theSameElementsInOrderAs List(
-          aTimeIntervalForDate("2019-03-18", "09:00", "19:00"),
-          aTimeIntervalForDate("2019-03-19", "09:30", "14:00"),
-          aTimeIntervalForDate("2019-03-19", "15:00", "19:00"),
-          aTimeIntervalForDate("2019-03-20", "09:30", "20:00"),
-          aTimeIntervalForDate("2019-03-21", "09:30", "19:00"),
-          aTimeIntervalForDate("2019-03-22", "09:30", "19:00"),
-          aTimeIntervalForDate("2019-03-23", "09:00", "14:00"),
-          aTimeIntervalForDate("2019-03-23", "15:00", "19:00")
-        )
-      }
-    }
-
-    "with exceptions" should {
-
-      "the exception is not in the first or last day" should {
-        val exceptions = Map(
-          parseDate("2019-03-18") -> List(
-            parseInterval("13:00", "16:00")
-          )
-        )
-
-        val schedule = Schedule(planning, exceptions, zoneId)
-
-        "compute 6 intervals between Friday 15 13:40 to Tuesday 19 13:40 INCLUDING and exception the 2019-03-18 between 13:00 and 16:00" in {
-          val s1 =
-            schedule.intervalsBetween(
-              parseZonedDateTime("2019-03-15", "13:40"),
-              parseZonedDateTime("2019-03-19", "13:40")
-            )
-          s1 should contain theSameElementsInOrderAs List(
-            aTimeIntervalForDate("2019-03-15", "13:40", "19:00"),
-            aTimeIntervalForDate("2019-03-16", "09:00", "14:00"),
-            aTimeIntervalForDate("2019-03-16", "15:00", "19:00"),
-            aTimeIntervalForDate("2019-03-18", "09:00", "13:00"),
-            aTimeIntervalForDate("2019-03-18", "16:00", "19:00"),
-            aTimeIntervalForDate("2019-03-19", "09:30", "13:40")
-          )
-        }
-      }
-
-      "the exception is during the first day" should {
-        val exceptions = Map(
-          parseDate("2019-03-15") -> List(
-            parseInterval("13:00", "16:00")
-          )
-        )
-
-        val schedule = Schedule(planning, exceptions, zoneId)
-
-        "compute 6 intervals between Friday 15 13:40 to Tuesday 19 13:40 INCLUDING and exception the  019-03-15 between 13:00 and 16:00" in {
-          val s1 =
-            schedule.intervalsBetween(
-              parseZonedDateTime("2019-03-15", "13:40"),
-              parseZonedDateTime("2019-03-19", "13:40")
-            )
-          s1 should contain theSameElementsInOrderAs List(
-            aTimeIntervalForDate("2019-03-15", "16:00", "19:00"),
-            aTimeIntervalForDate("2019-03-16", "09:00", "14:00"),
-            aTimeIntervalForDate("2019-03-16", "15:00", "19:00"),
-            aTimeIntervalForDate("2019-03-18", "09:00", "19:00"),
-            aTimeIntervalForDate("2019-03-19", "09:30", "13:40")
-          )
-        }
-      }
-
-      "the exception is during the last day" should {
-        val exceptions = Map(
-          parseDate("2019-03-19") -> List(
-            parseInterval("13:00", "16:00")
-          )
-        )
-
-        val schedule = Schedule(planning, exceptions, zoneId)
-
-        "compute 6 intervals between Friday 15 13:40 to Tuesday 19 13:40 INCLUDING and exception the 2019-03-19 between 13:00 and 16:00" in {
-          val s1 =
-            schedule.intervalsBetween(
-              parseZonedDateTime("2019-03-15", "13:40"),
-              parseZonedDateTime("2019-03-19", "13:40")
-            )
-          s1 should contain theSameElementsInOrderAs List(
-            aTimeIntervalForDate("2019-03-15", "13:40", "19:00"),
-            aTimeIntervalForDate("2019-03-16", "09:00", "14:00"),
-            aTimeIntervalForDate("2019-03-16", "15:00", "19:00"),
-            aTimeIntervalForDate("2019-03-18", "09:00", "19:00"),
-            aTimeIntervalForDate("2019-03-19", "09:30", "13:00")
-          )
-        }
-      }
-
-      "between a start and end the same day" should {
-        val exceptions = Map(
-          parseDate("2019-03-15") -> List(
-            parseInterval("14:00", "16:00")
-          )
-        )
-
-        val schedule = Schedule(planning, exceptions, zoneId)
-
-        "compute 2 intervals between Friday 15 13:40 to Friday 15 19:00 INCLUDING and exception the 2019-03-18 between 14:00 and 16:00" in {
-          val s1 =
-            schedule.intervalsBetween(
-              parseZonedDateTime("2019-03-15", "13:40"),
-              parseZonedDateTime("2019-03-15", "19:00")
-            )
-          s1 should contain theSameElementsInOrderAs List(
-            aTimeIntervalForDate("2019-03-15", "13:40", "14:00"),
-            aTimeIntervalForDate("2019-03-15", "16:00", "19:00")
-          )
-        }
-      }
-    }
-  }
-
-  def parseZonedDateTime(day: String, time: String): ZonedDateTime =
-    ZonedDateTime.parse(s"${day}T$time:00.000+01:00[$zoneId]")
-
-  def parseInterval(startTime: String, endTime: String) =
-    TimeInterval(
-      LocalTime.parse(s"$startTime:00"),
-      LocalTime.parse(s"$endTime:00")
-    )
-
-  def parseDate(date: String): LocalDate = LocalDate.parse(date)
-
-  def aTimeIntervalForDate(date: String, startTime: String, endTime: String) =
-    TimeIntervalForDate(
-      parseDate(date),
-      parseInterval(startTime, endTime)
-    )
 //
 //
 //  "Merging mergeTimeSegments" should {
@@ -260,7 +57,7 @@ class IntervalsSpec extends WordSpec with Matchers {
 
     "Will return Nil" should {
       "including is empty and excluding is 04:00 -> 11:00" in {
-        val excludingInterval = parseInterval("04:00", "11:00")
+        val excludingInterval = "04:00" - "11:00"
 
         val res = Schedule.cutExceptions(Nil, List(excludingInterval))
 
@@ -268,8 +65,8 @@ class IntervalsSpec extends WordSpec with Matchers {
       }
 
       "including is 05:00 -> 10:00 and excluding is 04:00 -> 11:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
-        val excludingInterval = parseInterval("04:00", "11:00")
+        val includingInterval = "05:00" - "10:00"
+        val excludingInterval = "04:00" - "11:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
@@ -277,8 +74,8 @@ class IntervalsSpec extends WordSpec with Matchers {
       }
 
       "including is 05:00 -> 10:00 and excluding is 05:00 -> 10:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
-        val excludingInterval = parseInterval("05:00", "10:00")
+        val includingInterval = "05:00" - "10:00"
+        val excludingInterval = "05:00" - "10:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
@@ -288,8 +85,8 @@ class IntervalsSpec extends WordSpec with Matchers {
 
     "Will return List(includingInterval)" should {
       "including is 05:00 -> 10:00 and excluding is 01:00 -> 04:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
-        val excludingInterval = parseInterval("01:00", "04:00")
+        val includingInterval = "05:00" - "10:00"
+        val excludingInterval = "01:00" - "04:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
@@ -297,8 +94,8 @@ class IntervalsSpec extends WordSpec with Matchers {
       }
 
       "including is 01:00 -> 04:00 and excluding is 05:00 -> 10:00" in {
-        val includingInterval = parseInterval("01:00", "04:00")
-        val excludingInterval = parseInterval("05:00", "10:00")
+        val includingInterval = "01:00" - "04:00"
+        val excludingInterval = "05:00" - "10:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
@@ -306,8 +103,8 @@ class IntervalsSpec extends WordSpec with Matchers {
       }
 
       "including is 01:00 -> 04:00 and excluding is 04:00 -> 10:00" in {
-        val includingInterval = parseInterval("01:00", "04:00")
-        val excludingInterval = parseInterval("04:00", "10:00")
+        val includingInterval = "01:00" - "04:00"
+        val excludingInterval = "04:00" - "10:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
@@ -317,93 +114,93 @@ class IntervalsSpec extends WordSpec with Matchers {
 
     "Will return including start -> excluding start segment" should {
       "including is 05:00 -> 10:00 and excluding is 06:00 -> 11:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
-        val excludingInterval = parseInterval("06:00", "11:00")
+        val includingInterval = "05:00" - "10:00"
+        val excludingInterval = "06:00" - "11:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
-        res should contain theSameElementsInOrderAs List(parseInterval("05:00", "06:00"))
+        res should contain theSameElementsInOrderAs List("05:00" - "06:00")
       }
 
       "including is 05:00 -> 10:00 and excluding is 06:00 -> 10:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
-        val excludingInterval = parseInterval("06:00", "10:00")
+        val includingInterval = "05:00" - "10:00"
+        val excludingInterval = "06:00" - "10:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
-        res should contain theSameElementsInOrderAs List(parseInterval("05:00", "06:00"))
+        res should contain theSameElementsInOrderAs List("05:00" - "06:00")
       }
     }
 
     "Will return excluding end -> including end interval" should {
       "including is 05:00 -> 10:00 and excluding is 04:00 -> 09:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
-        val excludingInterval = parseInterval("04:00", "09:00")
+        val includingInterval = "05:00" - "10:00"
+        val excludingInterval = "04:00" - "09:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
-        res should contain theSameElementsInOrderAs List(parseInterval("09:00", "10:00"))
+        res should contain theSameElementsInOrderAs List("09:00" - "10:00")
       }
 
       "including is 05:00 -> 10:00 and excluding is 05:00 -> 09:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
-        val excludingInterval = parseInterval("05:00", "09:00")
+        val includingInterval = "05:00" - "10:00"
+        val excludingInterval = "05:00" - "09:00"
 
         val res = Schedule.cutExceptions(List(includingInterval), List(excludingInterval))
 
-        res should contain theSameElementsInOrderAs List(parseInterval("09:00", "10:00"))
+        res should contain theSameElementsInOrderAs List("09:00" - "10:00")
       }
     }
 
     "Will return multiple intervals : (including.start -> excluding.start), (excluding.end -> including.end)" should {
 
       "including is 05:00 -> 10:00 and excluding is 06:00 -> 08:00" in {
-        val includingInterval = parseInterval("05:00", "10:00")
+        val includingInterval = "05:00" - "10:00"
 
-        val res = Schedule.cutExceptions(List(includingInterval), List(parseInterval("06:00", "08:00")))
+        val res = Schedule.cutExceptions(List(includingInterval), List("06:00" - "08:00"))
 
         res should contain theSameElementsInOrderAs List(
-          parseInterval("05:00", "06:00"),
-          parseInterval("08:00", "10:00")
+          "05:00" - "06:00",
+          "08:00" - "10:00"
         )
       }
 
       "Will return 3 intervals " in {
-        val includingInterval = parseInterval("05:00", "20:00")
+        val includingInterval = "05:00" - "20:00"
 
         val exceptions =
           List(
-            parseInterval("06:00", "08:00"),
-            parseInterval("09:30", "16:00"),
-            parseInterval("19:00", "22:00")
+            "06:00" - "08:00",
+            "09:30" - "16:00",
+            "19:00" - "22:00"
           )
 
         val res = Schedule.cutExceptions(List(includingInterval), exceptions)
 
         res should contain theSameElementsInOrderAs List(
-          parseInterval("05:00", "06:00"),
-          parseInterval("08:00", "09:30"),
-          parseInterval("16:00", "19:00")
+          "05:00" - "06:00",
+          "08:00" - "09:30",
+          "16:00" - "19:00"
         )
       }
 
       "Will return 4 intervals " in {
-        val includingInterval = parseInterval("05:00", "20:00")
+        val includingInterval = "05:00" - "20:00"
 
         val exceptions =
           List(
-            parseInterval("06:00", "08:00"),
-            parseInterval("09:30", "16:00"),
-            parseInterval("18:00", "19:00")
+            "06:00" - "08:00",
+            "09:30" - "16:00",
+            "18:00" - "19:00"
           )
 
         val res = Schedule.cutExceptions(List(includingInterval), exceptions)
 
         res should contain theSameElementsInOrderAs List(
-          parseInterval("05:00", "06:00"),
-          parseInterval("08:00", "09:30"),
-          parseInterval("16:00", "18:00"),
-          parseInterval("19:00", "20:00")
+          "05:00" - "06:00",
+          "08:00" - "09:30",
+          "16:00" - "18:00",
+          "19:00" - "20:00"
         )
       }
 
