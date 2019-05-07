b4f4efc7ee72f1b6a03ea5500ba10c30e34cac3e
Michel Daviot <michel@daviot.info>
2019-05-07 18:51:39 +0200

    cutoff implementation for jruby

--- jruby/src/main/scala/com/colisweb/jrubysnesshours/jruby/JRubyzSchedule.scala
+++ jruby/src/main/scala/com/colisweb/jrubysnesshours/jruby/JRubyzSchedule.scala
@@ -7,9 +7,14 @@ import com.colisweb.jrubysnesshours.core._
 import com.colisweb.jrubysnesshours.jruby.JRubyzSchedule._
 
 final class JRubyzSchedule private[jruby] (schedule: Schedule) {
-  def splitTimeSegments(startsAt: ZonedDateTime, endsAt: ZonedDateTime, hours: Long): Array[RubyTimeSegmentInterval] =
+  def splitTimeSegments(
+      startsAt: ZonedDateTime,
+      endsAt: ZonedDateTime,
+      hours: Long,
+      cutOff: Option[DoubleCutOff] = None
+  ): Array[RubyTimeSegmentInterval] =
     schedule
-      .splitTimeSegments(startsAt, endsAt, hours)
+      .splitTimeSegments(startsAt, endsAt, hours, cutOff)
       .map(RubyTimeSegmentInterval(_, schedule.timeZone))
       .toArray
 
--- jruby/src/test/scala/com/colisweb/jrubysnesshours/jruby/JRubyzScheduleApprobationSpec.scala
+++ jruby/src/test/scala/com/colisweb/jrubysnesshours/jruby/JRubyzScheduleApprobationSpec.scala
@@ -3,11 +3,13 @@ package com.colisweb.jrubysnesshours.jruby
 import java.time.ZonedDateTime
 
 import com.colisweb.Approbation
+import com.colisweb.jrubysnesshours.core.{CutOff, DoubleCutOff}
 import com.colisweb.jrubysnesshours.jruby.JRubyzSchedule.rubyWeekDayToJavaWeekDay
 import com.colisweb.jrubysnesshours.jruby.SampleSchedule.jrubySchedule
+import com.colisweb.jrubysnesshours.jruby.SpecUtils._
 import com.github.writethemfirst.approvals.utils.FunctionUtils
 
-import collection.JavaConverters._
+import scala.collection.JavaConverters._
 
 class JRubyzScheduleApprobationSpec extends Approbation {
 
@@ -18,6 +20,43 @@ class JRubyzScheduleApprobationSpec extends Approbation {
       2
     )
 
+    approver.verify(prettify(segments))
+  }
+
+  val cutOff = Some(
+    DoubleCutOff(
+      sameDay = CutOff(limit = "08:00".toLocalTime, firstAvailableTime = "16:00".toLocalTime),
+      nextDay = CutOff(limit = "12:00".toLocalTime, firstAvailableTime = "15:00".toLocalTime)
+    )
+  )
+
+  it should "not cut-off when before first time" in { approver =>
+    val segments = jrubySchedule.splitTimeSegments(
+      ZonedDateTime.parse("2019-05-06T06:50:39Z"),
+      ZonedDateTime.parse("2019-05-09T16:17:39Z"),
+      2,
+      cutOff
+    )
+    approver.verify(prettify(segments))
+  }
+
+  it should "cut-off when between 2 times" in { approver =>
+    val segments = jrubySchedule.splitTimeSegments(
+      ZonedDateTime.parse("2019-05-06T10:02:39Z"),
+      ZonedDateTime.parse("2019-05-09T16:17:39Z"),
+      2,
+      cutOff
+    )
+    approver.verify(prettify(segments))
+  }
+
+  it should "cut-off when after 2nd time" in { approver =>
+    val segments = jrubySchedule.splitTimeSegments(
+      ZonedDateTime.parse("2019-05-06T12:08:39Z"),
+      ZonedDateTime.parse("2019-05-09T16:17:39Z"),
+      2,
+      cutOff
+    )
     approver.verify(prettify(segments))
   }
 
