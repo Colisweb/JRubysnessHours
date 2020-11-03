d397ffd2f8706810261c672ba063279fe4e35c19
Scala Steward <gitlab@colisweb.com>
2020-11-03 08:32:26 +0000

    Update pprint to 0.5.9

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object TestVersions {
   lazy final val approval      = "1.1.1"
-  lazy final val pprint        = "0.5.6" // /!\ 0.6.0 has different formatting in scala 2.12 and 2.13 !
+  lazy final val pprint        = "0.5.9" // /!\ 0.6.0 has different formatting in scala 2.12 and 2.13 !
   lazy final val scalacheck    = "2.5.1"
   lazy final val scalatest     = "3.2.2"
   lazy final val scalatestplus = "3.1.0.0-RC2"
