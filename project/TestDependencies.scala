d07838339a0739bbb1387d677849bec9a60ae014
Scala Steward <gitlab@colisweb.com>
2020-11-17 05:11:39 +0000

    Update scalatest to 3.2.3

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -4,7 +4,7 @@ object TestVersions {
   lazy final val approval      = "1.1.1"
   lazy final val pprint        = "0.5.9" // /!\ 0.6.0 has different formatting in scala 2.12 and 2.13 !
   lazy final val scalacheck    = "2.5.1"
-  lazy final val scalatest     = "3.2.2"
+  lazy final val scalatest     = "3.2.3"
   lazy final val scalatestplus = "3.1.0.0-RC2"
 }
 
