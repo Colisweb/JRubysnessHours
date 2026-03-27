14dc63b04f41f7d75bc855ccef46f03bc9df6134
Scala Steward <gitlab@colisweb.com>
2026-03-27 23:22:00 +0000

    Update scalatest to 3.2.20

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -4,7 +4,7 @@ object TestVersions {
   lazy final val approval      = "1.3.2"
   lazy final val pprint        = "0.9.6"
   lazy final val scalacheck    = "2.12.0"
-  lazy final val scalatest     = "3.2.19"
+  lazy final val scalatest     = "3.2.20"
   lazy final val scalatestplus = "3.1.0.0-RC2"
 }
 
